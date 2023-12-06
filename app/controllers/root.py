ea5d69fb991ef7e623d52c6b7de01d6c84f4cbb4
UnizaMatBre <breznican1@stud.uniza.sk>
2023-12-06 23:21:40 +0100

    Added projectmaker action and view

--- app/controllers/root.py
+++ app/controllers/root.py
@@ -5,3 +5,7 @@ import base_controller
 class Controller(base_controller.BaseController):
     def GET_index(self):
         return self.retView()
+        
+        
+    def GET_projectmaker(self):
+        return self.retView()
---
+++ app/views/root/GET_projectmaker.py
@@ -0,0 +1,6 @@
+
+
+def View(data):
+    return r"""
+
+    """
