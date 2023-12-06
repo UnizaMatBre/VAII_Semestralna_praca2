855fbfe9717d1d247ba2f227993bb5af0b271c2d
UnizaMatBre <breznican1@stud.uniza.sk>
2023-12-06 15:23:36 +0100

    GET_index now returns view

--- app/controllers/root.py
+++ app/controllers/root.py
@@ -4,6 +4,6 @@ import base_controller
 
 class Controller(base_controller.BaseController):
     def GET_index(self):
-        return self.retJson({ "a": 1, "b": 2 })
+        return self.retView()
 
     
