10feab316579ecf484bb95545bc65aefc6d47714
UnizaMatBre <breznican1@stud.uniza.sk>
2023-12-07 00:22:15 +0100

    Added project creation formular

--- app/views/root/GET_projectmaker.py
+++ app/views/root/GET_projectmaker.py
@@ -1,6 +1,16 @@
 
 
 def View(data):
-    return r"""
-
-    """
+	return r"""
+	<form>
+	<ul>
+		<li><label for="project-name">Name of the project?</label>
+		<input type="text" id="project-name" /></li>
+		
+		<li><label for="project-desc">Description</label>
+		<input type="text" id="project-desc" /></li>
+	
+		<li><input type="submit" value="Create" /></li>
+	</ul>
+	</form>
+	"""
