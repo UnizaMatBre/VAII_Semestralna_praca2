936931d930b310f4430d031b329abe25949918f8
UnizaMatBre <breznican1@stud.uniza.sk>
2023-12-07 05:02:18 +0100

    Removed placeholder text

--- app/views/root/GET_index.py
+++ app/views/root/GET_index.py
@@ -28,12 +28,7 @@ def View(data):
 		
 		
 		<div id="content" class="main-content">
-			<h1>Lorem Ipsum</h1>
-		
-			
-			Praesent dolor nibh, interdum quis consectetur vitae, egestas et ante. Nulla interdum urna justo. Aliquam nisl ex, consequat eget porttitor quis, imperdiet non ante. Nulla et mauris eget neque pretium consequat. Morbi vulputate in est malesuada fringilla. Curabitur ac auctor magna. Nulla facilisi. Sed lobortis felis sed ultrices malesuada. Suspendisse tincidunt turpis turpis, nec malesuada ex dictum et. Fusce cursus nunc et sem rhoncus varius. Maecenas nec metus velit. Nulla tincidunt pellentesque maximus. Phasellus et luctus massa.
-			Quisque dignissim egestas eros sed auctor. Quisque suscipit augue vitae libero mattis, vel blandit tellus consectetur. Quisque sit amet ligula efficitur, convallis purus id, bibendum neque. Nunc rhoncus fringilla neque ac convallis. Donec et ex molestie, accumsan urna nec, malesuada lacus. Quisque gravida nulla lectus, vel viverra lacus tempor non. Vestibulum consequat ante sit amet nulla lacinia ullamcorper. Sed vehicula faucibus venenatis. Maecenas nec purus a metus sodales euismod in in est.
-			Donec ut libero a ex finibus accumsan ac at quam. Curabitur at est pulvinar, tempus lorem ut, facilisis urna. Nam sed porta massa. Aliquam arcu eros, porttitor vitae diam vel, auctor posuere libero. Mauris tempus pharetra diam at consequat. Aenean luctus tincidunt ligula sit amet maximus. Fusce sed odio consequat, blandit lacus vel, condimentum nisi. Fusce placerat, ex quis maximus euismod, augue quam suscipit sem, et aliquam odio odio non massa. Nulla facilisi. Vestibulum eu odio facilisis, laoreet lorem id, lacinia nunc. Maecenas in augue volutpat, mattis velit sed, gravida massa. 
+
 		</div>
 		
 		
