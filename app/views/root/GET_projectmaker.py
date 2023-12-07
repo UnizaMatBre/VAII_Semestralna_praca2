e68473fa198e207fdf8626f7c897e9d992932c06
UnizaMatBre <breznican1@stud.uniza.sk>
2023-12-07 01:14:45 +0100

    Implemented creation of projects from forms

--- app/views/root/GET_projectmaker.py
+++ app/views/root/GET_projectmaker.py
@@ -2,15 +2,40 @@
 
 def View(data):
 	return r"""
-	<form>
+	<form id="project-make-form">
 	<ul>
 		<li><label for="project-name">Name of the project?</label>
-		<input type="text" id="project-name" /></li>
+		<input type="text" id="project-name" required /></li>
 		
 		<li><label for="project-desc">Description</label>
-		<input type="text" id="project-desc" /></li>
+		<input type="text" id="project-desc" value="" /></li>
 	
 		<li><input type="submit" value="Create" /></li>
 	</ul>
 	</form>
+	
+	
+	<script>
+		// form submiter handler
+		document.getElementById("project-make-form").addEventListener("submit", function(event) {
+			event.preventDefault();
+			
+			let name = document.getElementById("project-name").value;
+			let desc = document.getElementById("project-desc").value;
+			
+			
+			fetch("projects", {
+				"method": "POST",
+				"body": JSON.stringify({
+					"name": name,
+					"description": desc
+				
+				})
+			})
+			.then( (data) => { 
+				console.log(data);
+			});
+		
+		});
+	</script>
 	"""
