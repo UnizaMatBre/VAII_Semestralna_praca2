fd912162ea64e7c907d5740086c1a6081f0fff2f
UnizaMatBre <breznican1@stud.uniza.sk>
2023-12-07 04:08:38 +0100

    Small changes

--- app/views/root/GET_projectmaker.py
+++ app/views/root/GET_projectmaker.py
@@ -16,26 +16,25 @@ def View(data):
 	
 	
 	<script>
-		// form submiter handler
-		document.getElementById("project-make-form").addEventListener("submit", function(event) {
-			event.preventDefault();
+	document.getElementById("project-make-form").addEventListener("submit", function(event) {
+	event.preventDefault();
 			
-			let name = document.getElementById("project-name").value;
-			let desc = document.getElementById("project-desc").value;
+	let name = document.getElementById("project-name").value;
+	let desc = document.getElementById("project-desc").value;
 			
 			
-			fetch("projects", {
-				"method": "POST",
-				"body": JSON.stringify({
-					"name": name,
-					"description": desc
+	fetch("projects", {
+		"method": "POST",
+		"body": JSON.stringify({
+			"name": name,
+			"description": desc
+		})
+	})
+	.then( (data) => { 
+		console.log(data);
 				
-				})
-			})
-			.then( (data) => { 
-				console.log(data);
-			});
-		
-		});
+		selectProject(data["rowid"])
+	});
+	});
 	</script>
 	"""
